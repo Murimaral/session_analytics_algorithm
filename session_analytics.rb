8391548cab02e1b5d85e8efea8c4522af4d48d30
root <root@DESKTOP-EHU0UEN.localdomain>
2021-12-01 02:09:46 -0300

    WIP funcao principal

--- session_analytics.rb
+++ session_analytics.rb
@@ -11,40 +11,100 @@ def session_parser(input_str)
   return JSON.parse(input_str)
 end
 
+def time_session_manager_2(hashes)
+  len = hashes.length - 1
+  startTime = 0
+  out_ary = []
+  out_pages = []
+  hashes.each.with_index do |hsh, i|
+    if startTime == 0
+      startTime = hsh.values[0]
+    end
+    out_pages << hsh.keys[0]
+    if hsh.values[0] - startTime > 600000
+      print i
+      session_hash = {
+      "duration": hashes[i-1].values[0] - startTime,
+      "pages": out_pages.uniq,
+      "startTime": startTime }
+      out_ary << session_hash
+      out_pages = []
+      startTime = 0
+      if i == len
+        print "dentro do elsif"
+        session_hash = {
+        "duration": hsh.values[0] - startTime,
+        "pages": out_pages.uniq,
+        "startTime": startTime }
+        out_ary << session_hash
+        out_pages = []
+        startTime = 0
+      end
+    elsif i == len
+      print "dentro do elsif"
+      session_hash = {
+      "duration": hsh.values[0] - startTime,
+      "pages": out_pages.uniq,
+      "startTime": startTime }
+      out_ary << session_hash
+      out_pages = []
+      startTime = 0
+    end
+  end
+  return out_ary
+end
 
 ### recebe um array de hashes com TIMESTAMPS, 
 ### calcula diferença de tempo e retorna um array de hashes estruturado
 def time_session_manager(hashes)
+  ### inicializaçao de variaveis uteis
   count = 0
   startTime = 0
   out_ary = []
   out_pages= []
   duration = 0
+  last_duration = 0
   session_hash = {}
-  hashes.each do |hsh|
-    ### 10 min em milisec 
+
+  ### iteração por cada hash do array
+  hashes.each.with_index do |hsh, i|
+    ### Se count é 0, o timestamp é o começo da sessao 
     if count == 0 
       startTime = hsh.values[0]
       count += 1
     end
+
+    ### adicionar urls para um array
     out_pages << hsh.keys[0]
-    last_duration = duration
+
+    ### armazenar duração de um ciclo anterior
+    if i > 0
+      last_duration = hashes[i-1].values[0] - startTime
+    end
+
+    ### atualizar a duração tempo atual - tempo de começo
     duration = hsh.values[0] - startTime
+  
     session_hash = {
       "duration": duration,
-      "pages": out_pages,
+      "pages": out_pages.uniq,
       "startTime": startTime }
+    
     if duration > 600000   ### 10 min em milisec 
       count = 0
       session_hash[:duration] = last_duration
       out_ary << session_hash
       out_pages = []
-      print session_hash
+      ### resetar os contadores de durações
+      last_duration = 0
+      duration = 0
+    elsif i == hashes.length - 1
+      out_ary << session_hash
+      out_pages = []
+      count = 0
+      duration = 0
+      last_duration = 0
     end
-    last_duration = 0
-  end
-  if session_hash[:duration] < 600000
-    out_ary << session_hash
   end
   return out_ary
 end
@@ -77,9 +137,8 @@ def session_analyzer(input_json)
   
   ### gerando o output final com uma última iteração
   output['sessionByUser'].each do |user, data|
-    output['sessionByUser'][user] = time_session_manager(data)
+    output['sessionByUser'][user] = time_session_manager_2(data)
   end
-
 
   print output
 end
